c4230c7cd1807af5440c93f8a4fc8f35a895a56e
Adrien <adrien.le.g.ll@mail.com>
2018-12-09 13:03:32 +0100

    added completion configs

---
+++ zsh/completion.sh
@@ -0,0 +1,19 @@
+# Use caching so that commands like apt and dpkg complete are useable
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path ~/.zsh/cache
+
+# Don't complete uninteresting users
+zstyle ':completion:*:*:*:users' ignored-patterns \
+        adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
+        clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
+        gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
+        ldap lp mail mailman mailnull man messagebus  mldonkey mysql nagios \
+        named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
+        operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
+        rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
+        usbmux uucp vcsa wwwrun xfs '_*'
+
+# ... unless we really want to.
+zstyle '*' single-ignored show
+
+zstyle ':completion:*' squeeze-slashes true
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -3,6 +3,7 @@ CURRENT_DIR=$(dirname $(readlink -f $0))
 # personal config ---------------------------------------------------------------------- #
 
 source $CURRENT_DIR/aliases_functions.sh
+source $CURRENT_DIR/completion.sh
 source $CURRENT_DIR/env_config.sh
 source $CURRENT_DIR/keybindings.sh
 source $CURRENT_DIR/prompt.sh
