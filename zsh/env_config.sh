24b31abb6614e40701d1496a9fed470ccf7c96ca
Adrien Le Gall <adrien.le.gall@zto-technology.com>
2018-12-10 14:05:09 +0100

    added history params

--- zsh/env_config.sh
+++ zsh/env_config.sh
@@ -9,6 +9,12 @@ setopt share_history
 setopt histignorealldups
 # ignore commands that start with space
 setopt hist_ignore_space
+# record timestamp of command in HISTFILE
+setopt extended_history
+# delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_expire_dups_first
+# ignore duplicated commands history list
+setopt hist_ignore_dups
 
 # Sort filenames numerically when it makes sense
 setopt numericglobsort
