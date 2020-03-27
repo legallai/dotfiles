40eb7853506064831ab5a48c2592f6a4ea7d86b2
Adrien <adrien.le.g.ll@gmail.com>
2020-03-27 22:58:38 +0100

    add a beybinding to go up a directory

--- zsh/keybindings.sh
+++ zsh/keybindings.sh
@@ -5,11 +5,19 @@ if [[ "${terminfo[kcbt]}" != "" ]]; then
     bindkey "${terminfo[kcbt]}" reverse-menu-complete
 fi
 
+function up-a-directory() {
+    BUFFER="cd .."
+    zle accept-line
+}
+
+zle -N up-a-directory
+
 bindkey '^[[1;5C' forward-word
 bindkey '^[[1;5D' backward-word
 bindkey '\eOA' history-substring-search-up
 bindkey '\eOB' history-substring-search-down
 bindkey '^A' beginning-of-line
+bindkey '^B' up-a-directory
 bindkey '^E' end-of-line
 bindkey '^K' vi-kill-eol
 bindkey '^U' backward-kill-line
