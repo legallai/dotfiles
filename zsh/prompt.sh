7d9489bdf5492fd3398e8b80d523307e303266af
Adrien <adrien.le.g.ll@gmail.com>
2021-01-09 11:41:49 +0100

    Add space after branch name in prompt

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -14,6 +14,7 @@ set_prompt() {
         STASH_QTY=$(git stash list | wc -l)
         echo -n ", "
         echo -n "%{$fg[blue]%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
+        echo -n " "
         if [ $GIT_CHANGES -gt 0 ]; then
             echo -n "%{$fg[red]%}+$(echo -n $GIT_CHANGES | awk '{$1=$1};1')%{$reset_color%}"
         fi
