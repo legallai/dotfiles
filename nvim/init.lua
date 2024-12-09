19f6a72fd50e3e14f675ea8f45f45e4c6c5e7fae
Adrien Le Gall <adrien.le.g.ll@gmail.com>
2024-12-09 12:43:47 +0100

    nvim enable mouse mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -106,7 +106,7 @@ vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 -- vim.opt.mouse = "a"
-vim.opt.mouse = ""
+vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in status line
 vim.opt.showmode = false
